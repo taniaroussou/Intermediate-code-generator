8a6943b9094dc1d34d22ba2e1648c6267083f983
Tania Roussou <57332815+taniaroussou@users.noreply.github.com>
2022-12-09 23:07:56 +0200

    Create Constants.java

---
+++ Project/utilities/Constants.java
@@ -0,0 +1,42 @@
+package utilities;
+
+public final class Constants {
+    public static final String DOUBLE_DECL = "DoubleDecl";
+    public static final String UNDECLARED = "@NotDeclared";
+    public static final String NON_VIRTUAL = "NonVirtual";
+    public static final String PRINTSTATEMENT = "PrintStatement";
+    public static final String EXPRESSION = "Expression";
+    public static final String STATEMENT = "Statement";
+    public static final String ASSIGNMENT = "Assignment";
+    public static final String ARRAY_ACCESS = "ArrayAccess";
+    public static final String INVALID_INDEX = "InvalidIndex"; 
+    public static final String ARRAY_LENGTH = "ArrayLength";
+    public static final String MEMBER_CALL = "MemberCall";
+    public static final String MEMBER_CALL_UNDECL = "MemberCallUndefined";
+    public static final String SUBTYPE = "SubType";
+    public static final String RETURN_TYPE = "ReturnType";
+    public static final String USE_ARGS = "UseArgs";
+    public static final String WRONG_ARGS = "WrongArgs";
+    public static final String METHOD_NOTFOUND = "MethodNotFound";
+    public static final String THIS_MAIN = "thisMain";
+    public static final String ARRAY_ALLOCATION = "ArrayAllocation";
+    public static final String ALLOCATION = "Allocation";
+
+    public static final String CLASS = "class";
+    public static final String VARIABLE = "variable";
+    public static final String METHOD_BODY = "methodBody";
+    public static final String FORMAL_PARAMETER = "formalParameter";
+    public static final String METHOD = "method";
+
+    public static final String INT = "int"; 
+    public static final String BOOLEAN = "boolean";
+    public static final String BOOLEAN_ARRAY = "boolean[]";
+    public static final String INT_ARRAY = "int[]";
+    public static final String STRING_ARRAY = "String[]";
+    public static final String VOID = "void";
+
+    public static final String TRUE = "1";
+    public static final String FALSE = "0";
+
+    public static final String THIS = "this";
+}
